1dcf26d891ee02d1fa60c745caca6c43fe75812e
EddieYe <sailingview@163.com>
2017-08-04 14:01:20 +0800

    Create Test7.java
    说明了文件名必须与public类的类名一致，（如果文件中有public类）
    这里可以看出如果有多个public类，那么文件名应该是哪个public类的呢？显然一个java源文件只能有一个public类。
    
    所以总结如下：一个Java源文件中最多只能有一个public类，当有一个public类时，源文件名必须与之一致，否则无法编译，如果源文件中没有一个public类，则文件名与类中没有一致性要求。至于main()不是必须要放在public类中才能运行程序。

---
+++ Test7.java
@@ -0,0 +1,25 @@
+import java;
+
+
+public class Test5
+{
+   int i = 1;
+
+}
+
+public class Test6
+{
+   int i = 2;
+
+public static void main(String[] args)
+{
+  System.out.println("main method");
+  
+}
+}
+
+如果运行Test7.java 报错：
+C:/javatest>javac Test7.java
+Test7.java:8: 类 Test6 是公共的，应在名为 Test6.java 的文件中声明
+ public class Test6
+        ^1 错误
